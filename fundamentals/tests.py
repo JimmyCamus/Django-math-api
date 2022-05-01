0c2147bb60bcc003a0f10046d0269ab03aedecc1
JimmyCamus <jeremy.camus.varela@gmail.com>
2022-05-01 10:59:01 -0400

    test: bad request tests added to fundamentals app

--- fundamentals/tests.py
+++ fundamentals/tests.py
@@ -8,6 +8,7 @@ TEST_CACHE_SETTING = {
 
 
 class FundamentalsTestCase(TestCase):
+    @override_settings(CACHES=TEST_CACHE_SETTING)
     def test_derivate(self):
         client = APIClient()
         response = client.post(
@@ -20,6 +21,18 @@ class FundamentalsTestCase(TestCase):
 
         self.assertEqual(json.loads(response.content), {'derivate': '2*x'})
 
+    @override_settings(CACHES=TEST_CACHE_SETTING)
+    def test_derivate_bad_request(self):
+        client = APIClient()
+        response = client.post(
+            '/api/fundamentals/derivate/',
+            {},
+            format='multipart'
+        )
+
+        self.assertEqual(json.loads(response.content), {'funct': ['This field is required.']})
+
+    @override_settings(CACHES=TEST_CACHE_SETTING)
     def test_integrate(self):
         client = APIClient()
         response = client.post(
@@ -31,3 +44,15 @@ class FundamentalsTestCase(TestCase):
         )
 
         self.assertEqual(json.loads(response.content), {'integrate': 'x**3/3'})
+
+    @override_settings(CACHES=TEST_CACHE_SETTING)
+    def test_integrate_bad_request(self):
+        client = APIClient()
+        response = client.post(
+            '/api/fundamentals/integrate/',
+            {
+            },
+            format='multipart'
+        )
+
+        self.assertEqual(json.loads(response.content), {'funct': ['This field is required.']})
